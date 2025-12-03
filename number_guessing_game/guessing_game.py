589c9c027292806bf7572fd9a17776560a2077dc
Jabir Gaadi <jgaadi@c3r10p8.1337.ma>
2025-12-03 11:35:19 +0100

    final update

--- number_guessing_game/guessing_game.py
+++ number_guessing_game/guessing_game.py
@@ -3,28 +3,44 @@ import sys
 
 
 def is_digit(value):
-	if (type(value) is int):
+	try:
+		int(value)
 		return True
-	return False
+	except ValueError:
+		return False
 
 
-def get_number_from_user():
-	number = input("*Enter a number between 1 and 50: ")
-	return int(number)
+def get_number_from_user(high, low):
+	number = input(f"\t\t*Enter a number between {low} and {high}: ")
+	return (number)
 
 
-random_number = random.randint(1, 50)
+print("\t\t------------------Welcome to the Guessing Game!-----------------")
+high = input("\n\tEnter the highest number you want to guess (default = 50): ")
+if (is_digit(high)):
+	high = int(high)
+else:
+	high = 50
+low = input("\tEnter the highest number you want to guess (default is 1): ")
+if (is_digit(low)):
+	low = int(low)
+	if (low >= high):
+		print("\t\t||Low number must be less than high number. Exiting the game.")
+		sys.exit(0)
+else:
+	low = 1
+random_number = random.randint(low, high)
 try_time = 0
 valid_number = 0
 
 while True:
-	input_user = get_number_from_user()
+	input_user = get_number_from_user(high, low)
 	while (not is_digit(input_user)):
 		valid_number += 1
-		print("*Enter a valid number next time")
-		input_user = get_number_from_user()
-		if (valid_number > 3):
-			print(f"\nYou have entered invalid input too many times. The correct number\
+		print("\t\t||Enter a valid number next time")
+		input_user = get_number_from_user(high, low)
+		if (valid_number == 2):
+			print(f"\n\t\tYou have entered invalid input too many times. The correct \
 			{random_number}")
 			sys.exit(0)
 	input_user = int(input_user)
@@ -38,8 +54,8 @@ while True:
 		try_time += 1
 		print(f"Guess {try_time}:{input_user}  ->  Correct!!!")
 		break
-	if (try_time > 7):
+	if (try_time > 6):
 		print(f"\nYou reach the maximum number of guesses. The correct number \
 		was {random_number}.")
 		sys.exit(0)
-print(f"\nTotal Guesses: {try_time}")
+print(f"\n\t\tTotal Guesses: {try_time}")
